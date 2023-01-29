a976a3fd99bc0e9f08e0c81a716137e27bfed61a
IreneChernobuk <irinka_buc@mail.ru>
2023-01-29 11:53:22 +0300

    worked with HerokuApp web-site and created some Test for add and delete task and tables task

---
+++ src/test/java/Urls.java
@@ -0,0 +1,4 @@
+public class Urls {
+    public static final String REGISTER_FORM_URL_ADD = "http://the-internet.herokuapp.com/add_remove_elements/";
+    public static final String REGISTER_FORM_URL_TABLE = "http://the-internet.herokuapp.com/tables";
+}
